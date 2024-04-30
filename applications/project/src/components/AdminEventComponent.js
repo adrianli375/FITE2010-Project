9502d9c9d589dfba0273d0ef18c998fb32de00ca
adrianli375 <adrian.li.9375@gmail.com>
2024-04-30 20:38:31 +0800

    updated admin event component to show remaining seats

--- applications/project/src/components/AdminEventComponent.js
+++ applications/project/src/components/AdminEventComponent.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { contractAddresses } from '../const/SmartContract.js';
+import { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import { contractAddresses, contractABI } from '../const/SmartContract.js';
 
 
 const AdminEventComponent = ({eventId, eventStar, eventTime, eventLocation, eventImgPath}) => {
     
+    // define state variables
+    const [seatsRemaining, setSeatsRemaining] = useState(null);
+    
     let contractIdx = eventId - 1;
     const eventContractAddress = contractAddresses[contractIdx];
+
+    // create a new Web3 instance
+    const web3 = new Web3(window.ethereum);
+
+    useEffect(() => {
+        getSeatsRemaining();
+    }, []);
+
+    const getSeatsRemaining = async () => {
+        if (window.ethereum) {
+            var contract = new web3.eth.Contract(contractABI, eventContractAddress);
+            let seats = await contract.methods.getTotalAvailableTickets().call();
+            setSeatsRemaining(seats);
+        }
+    };
     
     return (
         <div className="event">
@@ -18,6 +38,9 @@ const AdminEventComponent = ({eventId, eventStar, eventTime, eventLocation, even
                 <img src={eventImgPath} alt={eventStar} />
             </div>
             <br></br>
+            {seatsRemaining !== null && 
+                <p>Remaining Seats: <strong>{seatsRemaining}</strong></p>
+            }
             <a href={`https://sepolia.etherscan.io/address/${eventContractAddress}`} 
                 className="view-on-etherscan"
                 target="_blank" rel="noopener noreferrer">
