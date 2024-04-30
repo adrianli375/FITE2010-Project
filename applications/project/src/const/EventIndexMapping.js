6f0e79122a3a11213a7e14de24ec7a042d22e64d
adrianli375 <adrian.li.9375@gmail.com>
2024-04-30 20:21:49 +0800

    updated transfer page to support ticket transfer for different events

---
+++ applications/project/src/const/EventIndexMapping.js
@@ -0,0 +1,10 @@
+// provides event mapping from event name to the index
+const EventIndexMapping = {
+    "Panther Chan": 1,
+    "Kaho Hung": 2,
+    "Creamfield HK": 3,
+    "Midlife, Sing & Shine 2": 4
+};
+
+
+export default EventIndexMapping;
