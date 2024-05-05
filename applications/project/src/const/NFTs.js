1fb566dac5bfe1f2c6bdcb755edd98c988d5e24d
adrianli375 <adrian.li.9375@gmail.com>
2024-05-05 14:28:53 +0800

    update NFT list

--- applications/project/src/const/NFTs.js
+++ applications/project/src/const/NFTs.js
@@ -1,13 +1,49 @@
 const NFTs = [
     {
         eventId: 1,
+        name: "Panther Chan",
         nftList: [
-            "ipfs://QmSVLHHvL5RiskfagS1Wp5HZfDQqfji2e7QcwgL5GQif1x"
+            "ipfs://QmULP6zpTxJTEyjsVysMvFxxng5ZrZztmz8Z7wf81n5KNN/nft-metadata-panther-chan-1.json",
+            "ipfs://QmULP6zpTxJTEyjsVysMvFxxng5ZrZztmz8Z7wf81n5KNN/nft-metadata-panther-chan-2.json",
+            "ipfs://QmULP6zpTxJTEyjsVysMvFxxng5ZrZztmz8Z7wf81n5KNN/nft-metadata-panther-chan-3.json",
+            "ipfs://QmULP6zpTxJTEyjsVysMvFxxng5ZrZztmz8Z7wf81n5KNN/nft-metadata-panther-chan-4.json",
+            "ipfs://QmULP6zpTxJTEyjsVysMvFxxng5ZrZztmz8Z7wf81n5KNN/nft-metadata-panther-chan-5.json",
+            "ipfs://QmULP6zpTxJTEyjsVysMvFxxng5ZrZztmz8Z7wf81n5KNN/nft-metadata-panther-chan-6.json",
+            "ipfs://QmULP6zpTxJTEyjsVysMvFxxng5ZrZztmz8Z7wf81n5KNN/nft-metadata-panther-chan-7.json"
+        ]
+    },
+    {
+        eventId: 2,
+        name: "Kaho Hung",
+        nftList: [
+            "ipfs://QmUUbj3F5tzkkMAg2P7wLwtVLPxmLewPPzAEgYAE5tieVL/nft-metadata-kaho-hung-1.json",
+            "ipfs://QmUUbj3F5tzkkMAg2P7wLwtVLPxmLewPPzAEgYAE5tieVL/nft-metadata-kaho-hung-2.json",
+            "ipfs://QmUUbj3F5tzkkMAg2P7wLwtVLPxmLewPPzAEgYAE5tieVL/nft-metadata-kaho-hung-3.json"
+        ]
+    },
+    {
+        eventId: 3,
+        name: "Creamfield HK",
+        nftList: [
+            "ipfs://QmXudTRzkAL3G6vfye8bzcHQeJFz351uiAHUKU9WhsYT43/nft-metadata-creamfield-1.json",
+            "ipfs://QmXudTRzkAL3G6vfye8bzcHQeJFz351uiAHUKU9WhsYT43/nft-metadata-creamfield-2.json",
+            "ipfs://QmXudTRzkAL3G6vfye8bzcHQeJFz351uiAHUKU9WhsYT43/nft-metadata-creamfield-3.json"
+        ]
+    },
+    {
+        eventId: 4,
+        name: "Midlife, Sing & Shine 2",
+        nftList: [
+            "ipfs://QmNXKzBE3H3dTjgWXemGLXWGMFhhC5mrdLFEBMycDu9DmW/nft-metadata-midlife-1.json",
+            "ipfs://QmNXKzBE3H3dTjgWXemGLXWGMFhhC5mrdLFEBMycDu9DmW/nft-metadata-midlife-2.json",
+            "ipfs://QmNXKzBE3H3dTjgWXemGLXWGMFhhC5mrdLFEBMycDu9DmW/nft-metadata-midlife-3.json",
+            "ipfs://QmNXKzBE3H3dTjgWXemGLXWGMFhhC5mrdLFEBMycDu9DmW/nft-metadata-midlife-4.json",
+            "ipfs://QmNXKzBE3H3dTjgWXemGLXWGMFhhC5mrdLFEBMycDu9DmW/nft-metadata-midlife-5.json",
+            "ipfs://QmNXKzBE3H3dTjgWXemGLXWGMFhhC5mrdLFEBMycDu9DmW/nft-metadata-midlife-6.json",
+            "ipfs://QmNXKzBE3H3dTjgWXemGLXWGMFhhC5mrdLFEBMycDu9DmW/nft-metadata-midlife-7.json"
         ]
     }
 ];
-const TestNFT = "ipfs://QmSVLHHvL5RiskfagS1Wp5HZfDQqfji2e7QcwgL5GQif1x";
 
 
-// export default NFTs;
-export default TestNFT;
+export default NFTs;
